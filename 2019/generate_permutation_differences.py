820062d8862880e542bd940077e766fef7e90e35
Oscar Riveros <oscar.riveros@gmail.com>
2019-01-04 18:48:17 -0300

    Update generate_permutation_differences.py

--- 2019/generate_permutation_differences.py
+++ 2019/generate_permutation_differences.py
@@ -1,3 +1,4 @@
+# If anyone wants to generate some instance of the problem of "Permutation Reconstruction from Differences" here is a very simple script, you can put in the comments or something similar.
 # https://twitter.com/maxtuno/status/1081191522450792448
 if __name__ == '__main__':
     import sys
